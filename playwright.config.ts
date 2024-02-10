b88115bb44e84269ccb77887c61845732075e1f2
MrDotAstronaut <mrdotastronaut@gmail.com>
2024-02-10 23:23:22 +0530

    change reporter value

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: process.env.CI ? 'blob' : [['list'], ['html']],
+  reporter: process.env.CI ? 'blob' : 'html',
 
   use: {
     actionTimeout: 0,
